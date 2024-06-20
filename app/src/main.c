d3710ea641471a01b369288154233b4c2f596179
Joel Frazier <frazieje@gmail.com>
2024-06-19 22:16:24 -0400

    remove example drivers

--- app/src/main.c
+++ app/src/main.c
@@ -4,10 +4,7 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
-
-#include <app/drivers/blink.h>
 
 #include <app_version.h>
 
@@ -19,9 +16,6 @@
 #include "ndef_file_m.h"
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
-
-#define BLINK_PERIOD_MS_STEP 100U
-#define BLINK_PERIOD_MS_MAX  1000U
 
 static uint8_t ndef_msg_buf[CONFIG_NDEF_FILE_SIZE]; /**< Buffer for NDEF file. */
 
@@ -94,33 +88,6 @@ static void nfc_callback(void *context,
 
 int main(void)
 {
-	int ret;
-	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
-	const struct device *sensor, *blink;
-	struct sensor_value last_val = { 0 }, val;
-
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
-
-	sensor = DEVICE_DT_GET(DT_NODELABEL(example_sensor));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return 0;
-	}
-
-	blink = DEVICE_DT_GET(DT_NODELABEL(blink_led));
-	if (!device_is_ready(blink)) {
-		LOG_ERR("Blink LED not ready");
-		return 0;
-	}
-
-//	ret = blink_off(blink);
-//	if (ret < 0) {
-//		LOG_ERR("Could not turn off LED (%d)", ret);
-//		return 0;
-//	}
-
-	printk("Use the sensor to change LED blinking period\n");
-
     /* Initialize NVS. */
     if (ndef_file_setup() < 0) {
         printk("Cannot setup NDEF file!\n");
@@ -152,7 +119,8 @@ int main(void)
         return 0;
     }
 
-    printk("Starting NFC Writable NDEF Message example\n");
+    printk("Started NFC Writable NDEF emulation \n");
+
     while (true) {
         if (atomic_cas(&op_flags, FLASH_BUF_PREP_FINISHED,
                        FLASH_WRITE_STARTED)) {
@@ -165,35 +133,6 @@ int main(void)
         }
         k_sleep(K_MSEC(100));
     }
-//	while (1) {
-//		ret = sensor_sample_fetch(sensor);
-//		if (ret < 0) {
-//			LOG_ERR("Could not fetch sample (%d)", ret);
-//			return 0;
-//		}
-//
-//		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-//		if (ret < 0) {
-//			LOG_ERR("Could not get sample (%d)", ret);
-//			return 0;
-//		}
-//
-//		if ((last_val.val1 == 0) && (val.val1 == 1)) {
-//			if (period_ms == 0U) {
-//				period_ms = BLINK_PERIOD_MS_MAX;
-//			} else {
-//				period_ms -= BLINK_PERIOD_MS_STEP;
-//			}
-//
-//			printk("Proximity detected, setting LED period to %u ms\n",
-//			       period_ms);
-//			blink_set_period_ms(blink, period_ms);
-//		}
-//
-//		last_val = val;
-//
-//		k_sleep(K_MSEC(100));
-//	}
 	return 0;
 }
 
