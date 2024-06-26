1092398b5d28b08d846222c1f307c21d5c4e9285
Joel Frazier <joel.frazier@digitalturbine.com>
2024-06-26 15:40:06 -0400

    start enabling Matter

--- app/src/nfc.c
+++ app/src/nfc.c
@@ -5,28 +5,47 @@
 #include <nfc/ndef/msg.h>
 #include <nfc/t4t/ndef_file.h>
 
-#include "ndef_file_m.h"
-
 static uint8_t ndef_msg_buf[CONFIG_NDEF_FILE_SIZE]; /**< Buffer for NDEF file. */
+
+int ndef_file_default_message(uint8_t *buff, uint32_t *size)
+{
+    int err;
+    uint32_t ndef_size = nfc_t4t_ndef_file_msg_size_get(*size);
+
+    /* Encode URI message into buffer. */
+    err = nfc_ndef_uri_msg_encode(NFC_URI_HTTP_WWW,
+                                  m_url,
+                                  sizeof(m_url),
+                                  nfc_t4t_ndef_file_msg_get(buff),
+                                  &ndef_size);
+    if (err) {
+        return err;
+    }
+
+    err = nfc_t4t_ndef_file_encode(buff, &ndef_size);
+    if (err) {
+        return err;
+    }
+
+    *size = ndef_size;
+
+    return 0;
+}
 
 int start_nfc_ro(nfc_t4t_callback_t callback) {
     return -1;
 }
 
 int start_nfc_rw(nfc_t4t_callback_t callback) {
-    /* Initialize NVS. */
-    if (ndef_file_setup() < 0) {
-        printk("Cannot setup NDEF file!\n");
-        return 0;
-    }
 
-    if (ndef_file_load(ndef_msg_buf, sizeof(ndef_msg_buf)) < 0) {
-        printk("Cannot load NDEF file!\n");
-        return 0;
+    int err = ndef_file_default_message(ndef_msg_buf, &size);
+    if (err < 0) {
+        printk("Cannot create default message!\n");
+        return err;
     }
 
     /* Set up NFC */
-    int err = nfc_t4t_setup(callback, NULL);
+    err = nfc_t4t_setup(callback, NULL);
     if (err < 0) {
         printk("Cannot setup t4t library!\n");
         return 0;
